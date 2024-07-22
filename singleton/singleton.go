55d4403e5c0880dd2242f6b4c06827eb8e306a8f
turamant <tur1amant@gmail.com>
2024-07-22 13:08:20 +0300

    singleton -refactor

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -5,28 +5,29 @@ import (
     "sync"
 )
 
-type singleton struct {
+type Singleton struct {
     value int
 }
 
 var (
-    instance *singleton
+    instance *Singleton
     once     sync.Once
 )
 
-func GetInstance() *singleton {
+func GetInstance(value int) *Singleton {
     once.Do(func() {
-        instance = &singleton{value: 42}
+        instance = &Singleton{value: value}
     })
     return instance
 }
 
 func main() {
-    s1 := GetInstance()
-    s2 := GetInstance()
+    s1 := GetInstance(42)
+    s2 := GetInstance(24)
 
     fmt.Println("s1 address:", s1)
     fmt.Println("s2 address:", s2)
     fmt.Println("s1.value:", s1.value)
     fmt.Println("s2.value:", s2.value)
+    fmt.Println("s1 == s2:", s1 == s2)
 }
